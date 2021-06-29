f49e53c5d12b2025edc191ed64da5b7e3cc075ae
Breno3B <brenorm@gmail.com>
2021-06-29 19:17:39 -0300

    Inicializandfo Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer() {
+  return (
+    <div />
+  );
+}
+
+export default Footer;
--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/Header'
 
 function Comidas() {
   return (
     <div>
-      olar
+      ola
     </div>
   );
 }
